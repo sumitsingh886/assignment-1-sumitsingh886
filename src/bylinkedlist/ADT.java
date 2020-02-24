71bda8b39a7bb40789e07f528cd72ef4e0f262c3
Sumit singh <53615200+sumitsingh886@users.noreply.github.com>
2020-02-24 20:50:46 +0530

    create Person class

--- src/bylinkedlist/ADT.java
+++ src/bylinkedlist/ADT.java
@@ -1,4 +1,11 @@
 package bylinkedlist;
 
-public class ADT {
+public interface ADT<E> {
+    void add(Person data);
+
+    void remove(Person data);
+
+    Person search(String firstName);
+
+    void print();
 }
