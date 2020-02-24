2501ad83cc480de401b2253bd8346a362a2aa62f
Sumit singh <53615200+sumitsingh886@users.noreply.github.com>
2020-02-24 15:33:58 +0530

    create strings

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -1,5 +1,36 @@
 package Person;
 
 public class Person {
+    private String firstname;
+    private String lastname;
+    private String emailAddress;
+    private StringBuilder contactNumbers = new StringBuilder();
 
+    public static setFirstName(String firstname) {
+        this.firstName = firstname;
+    }
+
+    public static setLastName(String lastname) {
+        this.LastName = lastname;
+    }
+
+    public static setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getFirstName() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public StringBuilder getContactNumbers() {
+        return contactNumbers;
+    }
 }
