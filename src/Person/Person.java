4eee26e9228dcd04657d488aa750a045658ad18b
Sumit singh <53615200+sumitsingh886@users.noreply.github.com>
2020-02-24 18:49:17 +0530

    create Tasks class

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -1,4 +1,49 @@
 package Person;
 
+import Contacts.Contacts;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private Contacts<String> contacts;
+    private String email;
+
+    public Person(String firstName, String lastName, Contacts<String> contacts, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contacts = contacts;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Contacts<String> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(Contacts<String> contacts) {
+        this.contacts = contacts;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
