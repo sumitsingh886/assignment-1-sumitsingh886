6f5bd2b8e932946c0d79604dd4d90f5789a28978
Sumit singh <53615200+sumitsingh886@users.noreply.github.com>
2020-02-24 18:43:21 +0530

    create main class

--- src/ADT/ADT.java
+++ src/ADT/ADT.java
@@ -1,5 +1,12 @@
 package ADT;
 
-public class ADT {
+public interface ADT<E> {
+    void add(Person data);
+
+    void remove(Person data);
+
+    Person search(String firstName);
+
+    void print();
 
 }
